47ab67da14de3c08d3b11c4abf919c83ba7cbe4e
DanielGustavo <dg520589@gmail.com>
2021-07-31 23:06:23 -0300

    feat: first commit 🎉

---
+++ webpack.config.js
@@ -0,0 +1,101 @@
+const path = require('path');
+
+const WebpackWatchedGlobEntries = require('webpack-watched-glob-entries-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const RemoveFilesWebpackPlugin = require('remove-files-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+let entryObject = {};
+
+const paths = {
+  build: path.resolve(__dirname, 'public', 'dist'),
+  src: path.resolve(__dirname, 'src'),
+  public: path.resolve(__dirname, 'public'),
+};
+
+module.exports = {
+  entry: () => {
+    entryObject = WebpackWatchedGlobEntries.getEntries([
+      path.resolve(paths.src, '**', '!(_)*.@(js|mjs)'),
+      path.resolve(paths.src, '**', '!(_)*.@(css|sass|scss)'),
+    ])();
+
+    return entryObject;
+  },
+  resolve: {
+    extensions: ['.js', '.mjs'],
+  },
+  output: {
+    path: paths.build,
+    filename: ({ chunk }) => {
+      const entryName = chunk.name;
+
+      const entry = entryObject[entryName];
+      const entryExtension = path.extname(entry);
+
+      let filename = `${entryName}${entryExtension}`;
+
+      if (['.css', '.sass', '.scss'].includes(entryExtension)) {
+        filename += '.[REMOVE_AFTER_BUILD]';
+      }
+
+      return filename;
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|mjs)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.(css|sass|scss)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              url: false,
+            },
+          },
+          'postcss-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              additionalData: (content, loaderContext) => {
+                const { resourcePath } = loaderContext;
+
+                const relativePublicPath = path.relative(
+                  resourcePath,
+                  paths.public
+                );
+
+                return `$publicPath: "${relativePublicPath}";${content}`;
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  devtool: process.env.NODE_ENV === 'development' ? 'source-map' : undefined,
+  plugins: [
+    new WebpackWatchedGlobEntries(),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+    new RemoveFilesWebpackPlugin({
+      after: {
+        test: [
+          {
+            folder: paths.build,
+            method: (filename) => /\.\[REMOVE_AFTER_BUILD\]$/.test(filename),
+            recursive: true,
+          },
+        ],
+      },
+    }),
+  ],
+};
